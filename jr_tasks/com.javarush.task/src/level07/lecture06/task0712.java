31ae76bc19d1b717b4b46aac3248d209815a175a
Koc Grebeniuk <kgrebeniuk@gmail.com>
2020-11-20 15:48:55 +0200

    07 add

---
+++ jr_tasks/com.javarush.task/src/level07/lecture06/task0712.java
@@ -0,0 +1,37 @@
+package level07.lecture06;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/*
+Самые-самые
+*/
+
+public class task0712 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int min = 0;
+        int max = 0;
+        ArrayList<String> list = new ArrayList();
+        for (int i = 0; i < 10; i++) {
+            list.add(reader.readLine());
+        }
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(max).length() < list.get(i).length()) {
+                max = i;
+            }
+            if (list.get(min).length() > list.get(i).length()) {
+                min = i;
+            }
+        }
+        if (max < min) {
+            System.out.println(list.get(max));
+        } else
+            System.out.println(list.get(min));
+    }
+}
+
+
